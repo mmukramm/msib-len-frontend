c28381b5f0fc266369725595264f3a827837453e
mukramm_ <99308248+mmukramm@users.noreply.github.com>
2024-08-19 00:24:24 +0800

    `feat`: clean code

--- application/views/page/location.php
+++ application/views/page/location.php
@@ -98,10 +98,9 @@ $this->load->view('_layout/header', $title);
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script>
-    // Update modal with the location ID
     $('#confirmModal').on('show.bs.modal', function(event) {
-        var button = $(event.relatedTarget); // Button that triggered the modal
-        var locationId = button.data('id'); // Extract info from data-* attributes
+        var button = $(event.relatedTarget); 
+        var locationId = button.data('id'); 
         var modal = $(this);
         modal.find('#deleteId').val(locationId);
     });
